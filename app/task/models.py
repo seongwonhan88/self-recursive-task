26c4e7afef97b92ce22454364707f6216bb88f49
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-18 10:09:38 +0900

    first commit

---
+++ app/task/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    """
+    self-recursive model
+    """
+    task_title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now=True)
+    lower_task = models.ForeignKey('self', related_name='upper_task', null=True, on_delete=models.CASCADE)
+
+
+
